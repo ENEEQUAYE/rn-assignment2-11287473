fe91da2e325bff7f787e22740a993c3264914902
EMMANUEL NEEQUAYE <eneequaye006@st.ug.edu.gh>
2024-06-26 19:37:12 +0000

    Added tyle={styles.text} attribute to Text tag

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>My name is Emmnauel Neequaye</Text>
+      <Text style={styles.text}>My name is Emmnauel Neequaye</Text>
       <StatusBar style="auto" />
     </View>
   );
