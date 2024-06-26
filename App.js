2b2298b2375985e29f7ebcee64033d8519f8f019
EMMANUEL NEEQUAYE <eneequaye006@st.ug.edu.gh>
2024-06-26 20:14:21 +0000

    Corrected spelling of Emmanuel

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>My name is <Text style={styles.boldText}>Emmnauel Neequaye</Text></Text>
+      <Text style={styles.text}>My name is <Text style={styles.boldText}>Emmanuel Neequaye</Text></Text>
       <StatusBar style="auto" />
     </View>
   );
